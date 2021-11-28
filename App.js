9bdeea180f3e61b9617d02e6d830fe5d198bcfde
eric Zhang <ehz6xz@virginia.edu>
2021-11-27 23:10:25 -0500

    updated some stuff

--- App.js
+++ App.js
@@ -1,32 +1,91 @@
-import React, {useState, useEffect} from 'react'
-import {Text, View, Button, StyleSheet} from 'react-native'
-import Constants from 'expo-constants'
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import * as Facebook from 'expo-facebook';
+import { NavigationContainer } from '@react-navigation/native';
+import LoadingScreen from "./Components/LoadingScreen"
+import HomeScreen from './Components/HomeScreen'
+import * as SecureStore from 'expo-secure-store';
 
-export default function App(){
-  let [joke, setJoke] = useState("loading...")
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { loading: true, token: null };
+  }
 
-  useEffect(()=>{
-    let getJoke = async ()=> {
-      let response = await fetch("http://api.icndb.com/jokes/random")
-      let jokeObject = await response.json()
-      setJoke(jokeObject.value.joke)
-    };
-    getJoke();
-  }, [])
+  componentWillMount(){
+   setTimeout (()=>{this.checkForToken()},2000)
+  }
 
-  return (
-    <View style={styles.container}>
-    <Text>{joke}</Text>
-    </View>
-  )
+
+  //Check Async Storage if token is available
+  //If it is available set loading state to false 
+  async checkForToken(){
+     let token = await SecureStore.getItemAsync('token')
+     console.log(token)
+    this.setState({
+      token: token,
+      loading: false
+    })
+  }
+
+  //Write token to secure storage. 
+  async saveTokenToSecureStorage(token){
+     SecureStore.setItemAsync("token", token)
+     this.setState({
+       token: token
+     })
+  }
+
+
+  render() {
+    if(this.state.loading === true){
+      return(<LoadingScreen/>)
+    }else if(this.state.token === null){
+    return (
+        <View style={styles.container}>
+          <Button title="LogIn With Facebook" onPress={() => this.logIn()} />
+        </View>
+      );
+    }
+    else{
+      return(<HomeScreen/>)
+    }
+  }
+
+  async logIn() {
+    try {
+      //Seed documentation on course site at mobileappdev.teachable.com
+      //For default user names and passwords.
+      await Facebook.initializeAsync('184462529575747');
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        );
+        this.saveTokenToSecureStorage(token)
+        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 8,
-  }
-})
+    justifyContent: 'center',
+  },
+});
